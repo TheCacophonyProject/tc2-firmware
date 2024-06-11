fc076d852b6b3665cdaa387b075d6f7004b937de
gferraro <g.ferraro22@gmail.com>
2024-06-12 11:21:14 +1200

    put this back to 300

--- src/ext_spi_transfers.rs
+++ src/ext_spi_transfers.rs
@@ -190,10 +190,7 @@ impl ExtSpiTransfers {
             GLOBAL_PING_PIN.borrow(cs).replace(Some(ping_pin));
         });
 
-        //this should be way less, gp hack
-        //CHANGE IT
-        // CHANGE IT
-        let _ = alarm.schedule(MicrosDurationU32::micros(100000)).unwrap();
+        let _ = alarm.schedule(MicrosDurationU32::micros(300)).unwrap();
         alarm.enable_interrupt();
         critical_section::with(|cs| {
             GLOBAL_ALARM.borrow(cs).replace(Some(alarm));
